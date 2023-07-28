2246e236d5e5e29826894c9f641f7bb5d5520012
Timimah <44163985+Timimah@users.noreply.github.com>
2023-07-28 03:12:51 +0100

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { Faqs } from "./pages/Faqs";
+import { Home } from "./pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Services } from "./pages/Services";
+import { Shop } from "./pages/Shop/Shop";
+import { Cart } from "./pages/Shop/Cart";
+import { CheckOut } from "./pages/Shop/CheckOut";
+import { CartProvider } from "./pages/Shop/cartContext";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/faqs",
+    element: <Faqs />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/services",
+    element: <Services />,
+  },
+  {
+    path: "/shop",
+    element: <Shop />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/checkout",
+    element: <CheckOut />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CartProvider>
+        <RouterProvider router={router} />
+      </CartProvider>
     </div>
   );
 }
